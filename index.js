a941e3993b84de73ea8a1e271b3d486bda10f118
Ekjot Singh <workforekjot@gmail.com>
2022-09-14 13:36:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log("Hello World") 
+// This is a comment
+
+/* This is a 
+multi-line comment */
+/* Data Types:
+    undefined, null, boolean, string, symbol, number, and object
+*/
+
+var MyName = "Ekjot" // variable can be used everywhere
+
+MyName = 8
+
+let OurName = "We all" // let will only be used where it is declared
+
+const pi = 3.14 // variable that can never be changed
+
+var a;
+var b = 10;
+
+console.log(a)
+console.log(b)
+
+a = 15;
+
+console.log(a)
+
+b = a;
+
+console.log(a)
+console.log(b)
+
+//Unintialized Variables
+var a = 5;
+var b = 10;
+var c = "I'm a ";
+
+a = a + 1;
+b = b + 5;
+c = c + "string"
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Case sensitivity in JS
+
+// Declarations
+var StUdLycapVaR;
+var PRoperCamelCase;
+var TitleCaseOver;
+
+// Assignments
+StUdLycapVaR = 10;
+PRoperCamelCase = "A String";
+TitleCaseOver = 9000;
